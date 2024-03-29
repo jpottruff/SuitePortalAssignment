8b4d25a540e7463848df0874084c723bc1acd52d
Jeff Pottruff <jeffpottruff@gmail.com>
2024-03-29 17:31:07 -0400

    fix test

--- apps/suite-portal/src/app/services/maintenance-request.service.spec.ts
+++ apps/suite-portal/src/app/services/maintenance-request.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { MaintenanceRequestService } from './maintenance-request.service';
 
@@ -6,7 +7,9 @@ describe('MaintenanceRequestService', () => {
   let service: MaintenanceRequestService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(MaintenanceRequestService);
   });
 
