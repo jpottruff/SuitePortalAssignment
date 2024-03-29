ae8569bd608c6a8778e811c6af37eccbb79d12ff
Jeff Pottruff <jeffpottruff@gmail.com>
2024-03-29 15:18:18 -0400

    add form boilerplate

--- apps/suite-portal/src/app/home/home.component.ts
+++ apps/suite-portal/src/app/home/home.component.ts
@@ -1,21 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import { ALL_SERVICE_TYPES } from '@suiteportal/api-interfaces';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  ALL_SERVICE_TYPES,
+  MaintenanceRequest,
+} from '@suiteportal/api-interfaces';
 
 @Component({
   selector: 'pm-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
   serviceTypes = ALL_SERVICE_TYPES;
 
-  constructor() {
+  maintenanceRequestForm: FormGroup;
+
+  constructor(private readonly formBuilder: FormBuilder) {
     //
   }
 
   ngOnInit(): void {
-    //
+    this.maintenanceRequestForm = this.initializeForm();
   }
 
+  initializeForm(): FormGroup {
+    return this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      unitNumber: ['', Validators.required],
+      serviceType: ['', Validators.required],
+      summary: ['', Validators.required],
+      details: [''],
+    });
+  }
+
+  onSubmit() {
+    const req: MaintenanceRequest = this.maintenanceRequestForm.value;
+    // TODO - send to api
+    console.log(req);
+  }
 }
