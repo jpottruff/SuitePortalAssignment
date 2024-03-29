6de0e2dea323be2333d599a59be74dfd2e2324fa
Jeff Pottruff <jeffpottruff@gmail.com>
2024-03-29 17:28:26 -0400

    add http client module to app

--- apps/suite-portal/src/app/app.module.ts
+++ apps/suite-portal/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app.routing.module';
 import { SharedModule } from './shared.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     AppRoutingModule,
     BrowserAnimationsModule,
     BrowserModule,
+    HttpClientModule,
     SharedModule,
   ],
   declarations: [AppComponent],
