42c4e62f25580c1bb9592e8cd1c8bec5e518f00f
Jeff Pottruff <jeffpottruff@gmail.com>
2024-03-29 17:26:34 -0400

    add service logic (WIP)

--- apps/suite-portal/src/app/services/maintenance-request.service.ts
+++ apps/suite-portal/src/app/services/maintenance-request.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { MaintenanceRequest } from '@suiteportal/api-interfaces';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MaintenanceRequestService {
+  constructor(private readonly http: HttpClient) {}
 
-  constructor() { }
+  get API_ENDPOINT(): string {
+    return `${environment.apiUrl}/maintenance-requests`;
+  }
+
+  submitRequest(request: MaintenanceRequest) {
+    const url = `${this.API_ENDPOINT}/maintenance-requests`;
+    console.log(url, request);
+    // TODO - hook up backend
+    // this.http.post(url, request);
+  }
 }
