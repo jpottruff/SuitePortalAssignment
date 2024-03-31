10a1e024081c1c0acbe9b5d61cd5642d3c899659
Jeff Pottruff <jeffpottruff@gmail.com>
2024-03-31 17:39:23 -0400

    hook up request list (basic)

--- apps/suite-portal/src/app/admin/request-list/request-list.component.ts
+++ apps/suite-portal/src/app/admin/request-list/request-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { MaintenanceRequest } from '@suiteportal/api-interfaces';
+import { Observable } from 'rxjs';
+import { MaintenanceRequestService } from '../../services/maintenance-request.service';
 
 @Component({
   selector: 'sp-request-list',
   templateUrl: './request-list.component.html',
-  styleUrls: ['./request-list.component.css']
+  styleUrls: ['./request-list.component.css'],
 })
 export class RequestListComponent implements OnInit {
+  constructor(
+    private readonly maintenanceRequestService: MaintenanceRequestService
+  ) {}
 
-  constructor() { }
-
+  requests$: Observable<MaintenanceRequest[]>;
   ngOnInit(): void {
+    this.requests$ = this.maintenanceRequestService.getRequestList();
   }
-
 }
--- apps/suite-portal/src/app/services/maintenance-request.service.ts
+++ apps/suite-portal/src/app/services/maintenance-request.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MaintenanceRequest } from '@suiteportal/api-interfaces';
 import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -19,5 +20,10 @@ export class MaintenanceRequestService {
       // TODO -  handling
       console.log(res);
     });
+  }
+
+  getRequestList(): Observable<MaintenanceRequest[]> {
+    const url = `${this.API_ENDPOINT}`;
+    return this.http.get<MaintenanceRequest[]>(url);
   }
 }
