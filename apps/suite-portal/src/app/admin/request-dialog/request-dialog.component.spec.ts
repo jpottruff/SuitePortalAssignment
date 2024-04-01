20d15c5a1a7f3ad8d9bf804341c576029fc45269
Jeff Pottruff <jeffpottruff@gmail.com>
2024-03-31 20:43:36 -0400

    fix a test

--- apps/suite-portal/src/app/admin/request-dialog/request-dialog.component.spec.ts
+++ apps/suite-portal/src/app/admin/request-dialog/request-dialog.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RequestDialogComponent } from './request-dialog.component';
+import { SharedModule } from '../../shared.module';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 describe('RequestDialogComponent', () => {
   let component: RequestDialogComponent;
@@ -8,9 +10,13 @@ describe('RequestDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RequestDialogComponent ]
-    })
-    .compileComponents();
+      imports: [SharedModule],
+      declarations: [RequestDialogComponent],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
