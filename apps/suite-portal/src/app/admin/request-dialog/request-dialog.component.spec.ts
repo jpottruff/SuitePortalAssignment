28bff15000a9671d67909987d92ccc3c1f7e6622
Jeff Pottruff <jeffpottruff@gmail.com>
2024-04-01 20:40:51 -0400

    add request dialog tests

--- apps/suite-portal/src/app/admin/request-dialog/request-dialog.component.spec.ts
+++ apps/suite-portal/src/app/admin/request-dialog/request-dialog.component.spec.ts
@@ -3,6 +3,11 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RequestDialogComponent } from './request-dialog.component';
 import { SharedModule } from '../../shared.module';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MaintenanceRequest, ServiceType } from '@suiteportal/api-interfaces';
+
+const MOCK_DIALOG: Partial<MatDialogRef<any>> = {
+  close: jest.fn(),
+};
 
 describe('RequestDialogComponent', () => {
   let component: RequestDialogComponent;
@@ -13,7 +18,7 @@ describe('RequestDialogComponent', () => {
       imports: [SharedModule],
       declarations: [RequestDialogComponent],
       providers: [
-        { provide: MatDialogRef, useValue: {} },
+        { provide: MatDialogRef, useValue: MOCK_DIALOG },
         { provide: MAT_DIALOG_DATA, useValue: {} },
       ],
     }).compileComponents();
@@ -27,5 +32,33 @@ describe('RequestDialogComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('onCancelCLick()', () => {
+    it('should close the dialog', () => {
+      const functionSpy = jest.spyOn(MOCK_DIALOG, 'close');
+      component.onCancelClick();
+      expect(functionSpy).toHaveBeenCalled();
+    });
+  });
+
+  describe('onConfirmCLick()', () => {
+    it('should close the dialog, and emit the data with isClosed: true', () => {
+      const mockRequest: MaintenanceRequest = {
+        name: 'Patti',
+        email: 'patti@email.com',
+        unitNumber: '123',
+        serviceType: ServiceType.Electrical,
+        summary: 'my request',
+        details: 'further details',
+      };
+
+      const functionSpy = jest.spyOn(MOCK_DIALOG, 'close');
+      component.data = mockRequest;
+      component.onConfirmClick();
+      expect(functionSpy).toHaveBeenCalledWith(
+        expect.objectContaining({ isClosed: true })
+      );
+    });
   });
 });
